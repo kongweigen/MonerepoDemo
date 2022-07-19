cc0d24ad2515659765c974707b71cc630a5de1a3
kongweigen <421505648@qq.com>
2022-07-19 23:09:16 +0800

    预览代码提交

---
+++ packages/ui/packages/Button/docs/demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <k-button class="mr20" @click="test(1)">按钮1</k-button>
+  <k-button class="mr20" @click="test(2)">按钮2</k-button>
+  <k-button class="mr20" @click="test(3)">按钮3</k-button>
+</template>
+
+<script setup lang="ts">
+const test = (val: number) => console.log(val)
+</script>
+<style scoped lang="scss">
+.mr20 {
+  margin-right: 20px;
+}
+</style>
